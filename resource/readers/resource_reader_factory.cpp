94dd8d0cdfe6ffd11cfd82cd8d548767e43d3c5f
James Corbett <corbett8@llnl.gov>
2025-11-20 22:41:10 -0800

    readers: add jgf shorthand reader class
    Problem: there is no reader for reading JGFs without exclusive
    subtrees.
    
    Add a JGF reader subclass, devoid of implementation for now.

--- resource/readers/resource_reader_factory.cpp
+++ resource/readers/resource_reader_factory.cpp
@@ -18,6 +18,7 @@ extern "C" {
 #include "resource/readers/resource_reader_grug.hpp"
 #include "resource/readers/resource_reader_hwloc.hpp"
 #include "resource/readers/resource_reader_jgf.hpp"
+#include "resource/readers/resource_reader_jgf_shorthand.hpp"
 #include "resource/readers/resource_reader_rv1exec.hpp"
 
 namespace Flux {
@@ -26,7 +27,8 @@ namespace resource_model {
 bool known_resource_reader (const std::string &name)
 {
     bool rc = false;
-    if (name == "grug" || name == "hwloc" || name == "jgf" || name == "rv1exec")
+    if (name == "grug" || name == "hwloc" || name == "jgf" || name == "jgf_shorthand"
+        || name == "rv1exec")
         rc = true;
     return rc;
 }
@@ -42,6 +44,8 @@ std::shared_ptr<resource_reader_base_t> create_resource_reader (const std::strin
             reader = std::make_shared<resource_reader_hwloc_t> ();
         } else if (name == "jgf") {
             reader = std::make_shared<resource_reader_jgf_t> ();
+        } else if (name == "jgf_shorthand") {
+            reader = std::make_shared<resource_reader_jgf_shorthand_t> ();
         } else if (name == "rv1exec") {
             reader = std::make_shared<resource_reader_rv1exec_t> ();
         } else {
